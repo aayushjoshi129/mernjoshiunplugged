fff21f7341e1481dff32c75b1ddbd3fe553d3ab5
Aayush Joshi <aayushjoshi129@gmail.com>
2023-04-22 21:55:07 +0530

    first_commit_for_HOSTING_MERN_app

---
+++ app.js
@@ -0,0 +1,53 @@
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+dotenv.config({path:'./config.env'});
+require('./db/conn');
+
+app.use(express.json());
+
+// const User = require('./model/userSchema');
+
+// linking auth router file to main
+app.use(require('./router/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+// Middleware  -  interacts as intermediate  
+
+// const middleware = (req,res,next)=> {
+//    console.log('Hello Middleware');
+//    next()
+// }
+
+// middleware();
+
+app.get('/', (req,res)=>{
+   res.send(`Hello World from MERN_Learning_2023 Course`);
+});
+
+// app.get('/about', middleware, (req,res)=>{
+//    res.send("Hello World from About");
+// });
+
+app.get('/contact', (req,res)=>{
+   res.send("Hello World from contact");
+});
+
+app.get('/login', (req,res)=>{
+   res.send("Hello World from login");
+});
+
+app.get('/register', (req,res)=>{
+   res.send("Hello World from register");
+});
+
+if(process.env.NODE_ENV=='production'){
+   app.use(express.static("client/build")); 
+}
+
+app.listen(PORT, ()=>{
+   console.log(`Server is running at port number ${PORT}`);
+})
